c8307da5e790c2eeb98d805ae90b652516ad7eb8
Zach Byers <byerszach@gmail.com>
2018-02-01 20:31:48 -0700

    timer complete

--- Egg Timer/ViewController.swift
+++ Egg Timer/ViewController.swift
@@ -10,6 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  
+  var timer = Timer()
+  
+  @IBOutlet weak var timerLabel: UILabel!
+  
+  @objc func processTimer() {
+    if let time = Int(timerLabel.text!) {
+      if time == 0 {
+        timer.invalidate()
+      } else {
+        timerLabel.text = String(time - 1)
+      }
+    }
+  }
+  
+  @IBAction func pauseButton(_ sender: Any) {
+    timer.invalidate()
+  }
+  
+  @IBAction func playButton(_ sender: Any) {
+    timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.processTimer), userInfo: nil, repeats: true)
+  }
+  
+  @IBAction func resetButton(_ sender: Any) {
+    timerLabel.text = "210"
+  }
+  
+  @IBAction func minusTen(_ sender: Any) {
+    if let time = Int(timerLabel.text!) {
+      if time >= 10 {
+        timerLabel.text = String(time - 10)
+      }
+    }
+  }
+  
+  @IBAction func plusTen(_ sender: Any) {
+    if let time = Int(timerLabel.text!) {
+      timerLabel.text = String(time + 10)
+    }
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
